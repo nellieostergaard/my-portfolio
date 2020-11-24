d14bd1a67fd0687bbd8502a0734d9240d240ff1b
Nellie Östergaard <nellieostergaard@github.com>
2020-11-24 21:43:00 +0100

    animated name + removed some initial content

--- my-portfolio/src/store/index.js
+++ my-portfolio/src/store/index.js
@@ -4,7 +4,26 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    fullName: [
+      { letter: "N", style: "--i:1;" },
+      { letter: "e", style: "--i:2;" },
+      { letter: "l", style: "--i:3;" },
+      { letter: "l", style: "--i:4;" },
+      { letter: "i", style: "--i:5;" },
+      { letter: "e", style: "--i:6;" },
+      { letter: "Ö", style: "--i:7;" },
+      { letter: "s", style: "--i:8;" },
+      { letter: "t", style: "--i:9;" },
+      { letter: "e", style: "--i:10;" },
+      { letter: "r", style: "--i:11;" },
+      { letter: "g", style: "--i:12;" },
+      { letter: "a", style: "--i:13;" },
+      { letter: "a", style: "--i:14;" },
+      { letter: "r", style: "--i:15;" },
+      { letter: "d", style: "--i:16;" }
+    ]
+  },
   mutations: {},
   actions: {},
   modules: {}
