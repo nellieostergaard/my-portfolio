63ad7e93bd10942ded0bb01d83a482efd4cb5363
Nellie Östergaard <nellieostergaard@github.com>
2020-12-12 13:24:57 +0100

    change background

--- my-portfolio/src/components/ClockComponent.vue
+++ my-portfolio/src/components/ClockComponent.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="clock">
-  <div class="clock__second"></div>
-  <div class="clock__minute"></div>
+    <div class="clock__second"></div>
+    <div class="clock__minute"></div>
     <div class="clock__hour"></div>
-  <div class="clock__axis"></div>
+    <div class="clock__axis"></div>
     <section class="clock__indicator"></section>
     <section class="clock__indicator"></section>
     <section class="clock__indicator"></section>
     <section class="clock__indicator"></section>
-</div>
+  </div>
 </template>
 <script>
 export default {
