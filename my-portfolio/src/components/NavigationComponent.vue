46ccdf01cd78e0709859599b5daadf6927a7ad98
Nellie Östergaard <nellieostergaard@github.com>
2021-01-04 11:37:32 +0100

    new navbar + responsive

--- my-portfolio/src/components/NavigationComponent.vue
+++ my-portfolio/src/components/NavigationComponent.vue
@@ -1,19 +1,16 @@
 <template>
   <nav class="menu">
-    <ol>
-      <li class="menu-item"><router-link to="/">Home</router-link></li>
-      <li class="menu-item">
-        <a>Projects</a>
-        <ol class="sub-menu">
-          <li class="menu-item">
-            <router-link to="/project-1">Dashboard</router-link>
-          </li>
-          <li class="menu-item">
-            <router-link to="/project-2">Drag and Drop</router-link>
-          </li>
-        </ol>
-      </li>
-      <li class="menu-item"><router-link to="/about">About me</router-link></li>
-    </ol>
-  </nav>
+  <ol>
+    <li class="menu-item"><router-link to="/">Home</router-link></li>
+    <li class="menu-item"><router-link to="/about">About me</router-link></li>
+    <li class="menu-item">
+      <a>Projects</a>
+      <ol class="sub-menu">
+        <li class="menu-item"><router-link to="/project-1">Dashboard</router-link></li>
+        <li class="menu-item"><router-link to="/project-2">Drag and Drop</router-link></li>
+      </ol>
+    </li>
+    <li class="menu-item"><a href="#0">Contact</a></li>
+  </ol>
+</nav>
 </template>
